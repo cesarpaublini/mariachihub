a6121f61515e177c958485b199250115e626a947
Cesar Paublini <cesar.paublini001@gmail.com>
2025-07-13 09:11:37 -0400

    add missing key check for stripe secret key

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,6 +1,11 @@
-// lib/stripe.ts
 import Stripe from 'stripe'
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+const stripeSecretKey = process.env.STRIPE_SECRET_KEY
+
+if (!stripeSecretKey) {
+  throw new Error('Missing STRIPE_SECRET_KEY')
+}
+
+export const stripe = new Stripe(stripeSecretKey, {
   apiVersion: '2025-06-30.basil',
 })
