198e583c6308e41768c96a13b92c3ae1f16eb6ae
Cesar Paublini <cesar.paublini001@gmail.com>
2025-07-14 16:07:56 -0400

    Added Google Analytics Implemented

--- src/app/api/save-contact-message/route.ts
+++ src/app/api/save-contact-message/route.ts
@@ -6,7 +6,6 @@ import { supabase } from '../../../lib/supabaseClient';
 
 export async function POST(req: Request) {
   const resend = new Resend(process.env.RESEND_API_KEY!); // Make sure this is in .env.local
-
   const data = await req.json();
   const { name, email, phone, subject, message } = data;
 
